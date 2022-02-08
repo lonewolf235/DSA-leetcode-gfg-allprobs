566c1bbefdac5360d7b49412f00c84142bc39b95
Prakash Veer Singh Tomar <gagantomar235@gmail.com>
2022-02-09 00:14:10 +0530

    Time: 3 ms (97.50%), Space: 47.1 MB (65.12%) - LeetHub

--- 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.java
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.java
@@ -1,19 +1,3 @@
-// class Solution {
-//      static int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
-//         int count = 0;
-//         int i = 0;
-//         if (ruleKey.equals("color"))
-//             i = 1;
-//         else if (ruleKey.equals("name"))
-//             i = 2;
-
-//         for (List<String> item : items) {
-//             if (item.get(i).equals(ruleValue))
-//                 count++;
-//         }
-//         return count;
-//     }
-// }
 class Solution {
     public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
         char ch = ruleKey.charAt(0);
