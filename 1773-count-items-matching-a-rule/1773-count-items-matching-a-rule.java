0231a0433079727a3d7e3e5a466e2cb8f9d53fea
Prakash Veer Singh Tomar <gagantomar235@gmail.com>
2022-02-09 00:13:52 +0530

    Time: 5 ms (54.14%), Space: 56.6 MB (12.62%) - LeetHub

--- 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.java
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.java
@@ -1,15 +1,42 @@
-class Solution {
-     static int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
-        int count = 0;
-        int i = 0;
-        if (ruleKey.equals("color"))
-            i = 1;
-        else if (ruleKey.equals("name"))
-            i = 2;
+// class Solution {
+//      static int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+//         int count = 0;
+//         int i = 0;
+//         if (ruleKey.equals("color"))
+//             i = 1;
+//         else if (ruleKey.equals("name"))
+//             i = 2;
 
-        for (List<String> item : items) {
-            if (item.get(i).equals(ruleValue))
-                count++;
+//         for (List<String> item : items) {
+//             if (item.get(i).equals(ruleValue))
+//                 count++;
+//         }
+//         return count;
+//     }
+// }
+class Solution {
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        char ch = ruleKey.charAt(0);
+        int count = 0;
+            for (int i = 0; i < items.size(); i++){
+                if (ch == 't'){
+                    if (items.get(i).get(0).equals(ruleValue)){
+                        count++;
+                    }
+                }
+                if (ch == 'c'){
+                    if (items.get(i).get(1).equals(ruleValue)){
+                        count++;
+                    }
+                }
+                
+                if (ch == 'n'){
+                    if (items.get(i).get(2).equals(ruleValue)){
+                        count++;
+                    }
+                }
+                
+            
         }
         return count;
     }
