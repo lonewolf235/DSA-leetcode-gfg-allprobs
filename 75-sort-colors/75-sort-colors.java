b5ac60c18c9984467f21e3a6ebb84c39c0d00762
Prakash Veer Singh Tomar <gagantomar235@gmail.com>
2022-07-25 13:06:04 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (20.22%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,9 +1,19 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int len=nums.length;
-       Arrays.sort(nums);
-        for(int k:nums){
-            System.out.print(k+",");
+        int count=0,count1=0;
+        for(int i =0;i<nums.length;i++){
+            if(nums[i]==0)
+                count+=1;
+            if(nums[i]==1)
+                count1+=1;
         }
+            for(int i=0;i<nums.length;i++){
+                if(i<count)
+                    nums[i]=0;
+                else if(i<count1+count)
+                    nums[i]=1;
+                else 
+                    nums[i]=2;
+            }
     }
 }
