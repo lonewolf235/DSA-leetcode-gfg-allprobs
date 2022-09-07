d8cb9cb8bf5d2460aec5dc0495021eefb9f15bbe
Prakash Veer Singh Tomar <gagantomar235@gmail.com>
2022-09-07 11:07:14 +0530

    Time: 15 ms (5.09%), Space: 81.4 MB (28.50%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.java
+++ 11-container-with-most-water/11-container-with-most-water.java
@@ -1,27 +1,18 @@
 class Solution {
     public int maxArea(int[] height) {
-
-        // Intially the max area is 0
         int maxArea = 0; 
-        // The First pointer is started at 0th index
         int pointer1 = 0; 
-        // The Second pointer is started at the last index
         int pointer2 = height.length - 1; 
-        
-        // Loop till the pointers are inside the range given
         while(pointer1 < pointer2){
-            // If the value at the pointer1 is smaller, then
             if(height[pointer1] < height[pointer2]){
                 maxArea = Math.max(maxArea, height[pointer1] * (pointer2 - pointer1));
-                pointer1 += 1; // Decrease the index of pointer1
+                pointer1 += 1; 
             }
-            // If the value at the pointer2 is smaller, then
             else{
                 maxArea = Math.max(maxArea, height[pointer2] * (pointer2 - pointer1));
-                pointer2 -= 1; // Decrease the index of pointer2
+                pointer2 -= 1;
             }
         }
-        // Return the instance that gave us the maximum value
         return maxArea;
     }
 }
