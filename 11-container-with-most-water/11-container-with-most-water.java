fb63c49d1c752827966ca2b3f9d38981e5559126
Prakash Veer Singh Tomar <gagantomar235@gmail.com>
2022-09-07 11:05:18 +0530

    Time: 4 ms (84.42%), Space: 73.7 MB (65.70%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.java
+++ 11-container-with-most-water/11-container-with-most-water.java
@@ -1,23 +1,27 @@
 class Solution {
     public int maxArea(int[] height) {
-        int leftPointer = 0;
-        int rightPointer = height.length - 1;
-        int maxWaterInContainer = 0;
-        while(leftPointer < rightPointer){
-            
-            int waterContained = 0;
-            
-            if(height[leftPointer] < height[rightPointer]){
-                waterContained = height[leftPointer] * (rightPointer - leftPointer);
-                leftPointer++;
-            }else{
-                waterContained = height[rightPointer] * (rightPointer - leftPointer);
-                rightPointer--;
-            }
-            
-            maxWaterInContainer = Math.max(maxWaterInContainer, waterContained);
-        }
-        return maxWaterInContainer;
+
+        // Intially the max area is 0
+        int maxArea = 0; 
+        // The First pointer is started at 0th index
+        int pointer1 = 0; 
+        // The Second pointer is started at the last index
+        int pointer2 = height.length - 1; 
         
+        // Loop till the pointers are inside the range given
+        while(pointer1 < pointer2){
+            // If the value at the pointer1 is smaller, then
+            if(height[pointer1] < height[pointer2]){
+                maxArea = Math.max(maxArea, height[pointer1] * (pointer2 - pointer1));
+                pointer1 += 1; // Decrease the index of pointer1
+            }
+            // If the value at the pointer2 is smaller, then
+            else{
+                maxArea = Math.max(maxArea, height[pointer2] * (pointer2 - pointer1));
+                pointer2 -= 1; // Decrease the index of pointer2
+            }
+        }
+        // Return the instance that gave us the maximum value
+        return maxArea;
     }
 }
