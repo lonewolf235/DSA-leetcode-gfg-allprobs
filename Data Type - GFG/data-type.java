9cc2bdea73d60404ea3389c6ed76790430bf314e
Prakash Veer Singh Tomar <gagantomar235@gmail.com>
2023-06-17 12:38:23 +0530

    Added solution - LeetHub

---
+++ Data Type - GFG/data-type.java
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            sc.nextLine();
+            String s = sc.next();
+            Solution obj = new Solution();
+            int ans = obj.dataTypeSize(s);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static int dataTypeSize(String str) {
+        if(str.equals("Integer")||str.equals("Float"))
+        return 4;
+        if(str.equals("Long")||str.equals("Double"))
+        return 8;
+        return 1;
+        
+    }
+}
